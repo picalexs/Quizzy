63ac790917c6e62e1057d65834f6cde7030b451b
Theo3883 <teosandu88@gmail.com>
2025-04-02 19:32:42 +0300

    codebase finished

--- backend/src/main/java/com/backend/controller/UserController.java
+++ backend/src/main/java/com/backend/controller/UserController.java
@@ -6,6 +6,7 @@ import com.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,7 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<Collection<User>> getAllUsers() {
+
         return ResponseEntity.ok(userService.getAllUsers());
     }
 }
