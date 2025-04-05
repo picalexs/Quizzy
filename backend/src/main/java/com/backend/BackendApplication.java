2110669296c1b12ca55aa7bf35b9027d4e6bd6b1
Theo3883 <teosandu88@gmail.com>
2025-04-05 18:05:32 +0300

    PDF to Text done
    # Conflicts:
    #	backend/src/main/java/com/backend/controller/UserController.java

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -1,11 +1,19 @@
 package com.backend;
 
+import com.backend.flashCardGenerator.PDFtoText;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(BackendApplication.class, args);
+        //PDFtoText pdfText = new PDFtoText();
+        String courseName = "graph";
+        String courseNumber = "test";
+        String text = PDFtoText.generateTextFromPDF(courseName, courseNumber);
+        System.out.println("Extracted text: " + text);
+
     }
 }
