53ec4fdcec6d95f45e3bdfcfed2a71286eebfa61
Darie Mihnea <dmihnead15@gmail.com>
2025-04-07 05:22:45 -0700

    updated code

--- backend/src/main/java/com/backend/model/Course.java
+++ backend/src/main/java/com/backend/model/Course.java
@@ -1,6 +1,12 @@
 package com.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Set;
 
 @Entity
