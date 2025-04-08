40fcd72b1b9788357f76c97ff8882063c103944e
Theo3883 <teosandu88@gmail.com>
2025-04-08 08:13:38 +0300

    switched class from a package to another

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -1,6 +1,6 @@
 package com.backend;
 
-import com.backend.flashCardGenerator.PDFtoText;
+import com.backend.utils.PDFtoText;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- backend/src/main/java/com/backend/utils/PDFtoText.java
+++ backend/src/main/java/com/backend/utils/PDFtoText.java
@@ -1,4 +1,4 @@
-package com.backend.flashCardGenerator;
+package com.backend.utils;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
