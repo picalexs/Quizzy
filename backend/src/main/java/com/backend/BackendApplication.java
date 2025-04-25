979eec9f02c8a6d6c39fcfaa55c62dc14fe948a9
Theo3883 <teosandu88@gmail.com>
2025-04-25 17:01:52 +0300

    removed .idea

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -14,5 +14,6 @@ public class BackendApplication {
         //String text = PDFtoText.generateTextFromPDF(courseName, courseNumber);
         //System.out.println("Extracted text: " + text);
 
+
     }
 }
